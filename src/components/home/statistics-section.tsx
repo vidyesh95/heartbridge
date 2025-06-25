6ccf7af67b6c89e3e6d903e45e3eea216997c0ea
vinit-churi <vinitchuri@hotmail.com>
2025-06-25 16:18:53 +0530

    Refine StatisticsSection: increase padding for improved layout and visual balance

--- src/components/home/statistics-section.tsx
+++ src/components/home/statistics-section.tsx
@@ -1,6 +1,6 @@
 export default function StatisticsSection() {
     return (
-        <section className={"flex flex-col items-center justify-center gap-8 md:gap-16 px-4 md:px-0 py-16"}>
+        <section className={"flex flex-col items-center justify-center gap-8 md:gap-16 px-4 md:px-0 py-32"}>
             <div className={"flex flex-col md:flex-row gap-4 md:gap-16"}>
                 <div>
                     <h4 className={"text-4xl text-primary text-center"}>50K+</h4>
