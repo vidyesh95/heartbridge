2eadcc673a0d4409feffac8c175526aab07aaee5
vinit-churi <vinitchuri@hotmail.com>
2025-06-28 20:01:37 +0530

    Enhance Contact page: add Card component with "Send us a message" section and placeholder content.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,6 @@
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
 export default function Contact() {
     return (
         <section className={"flex flex-col items-center justify-center gap-16 px-4 md:px-0 py-32 bg-secondary"}>
@@ -6,6 +9,20 @@ export default function Contact() {
                 <p className={"max-w-xl text-muted-foreground"}>
                     We&apos;re here to help you on your matrimonial journey
                 </p>
+            </div>
+            <div className={"grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8"}>
+                <div></div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Send us a message</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Button>Send Message</Button>
+                    </CardFooter>
+                </Card>
             </div>
         </section>
     )
