c3c56dd0f761d2f4719171761055abe90f858945
vinit-churi <vinitchuri@hotmail.com>
2025-06-28 22:23:39 +0530

    Enhance Contact page: add input fields for user details and message in "Send us a message" section.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,8 @@
 import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {Clock, Mail, MapPin, MessageCircle, Phone} from "lucide-react";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
 
 export default function Contact() {
     return (
@@ -11,7 +13,7 @@ export default function Contact() {
                     We&apos;re here to help you on your matrimonial journey
                 </p>
             </div>
-            <div className={"w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8"}>
+            <div className={"w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 items-start"}>
                 <div className={"space-y-4 md:space-y-8"}>
                     <h5 className={"text-2xl md:text-3xl text-card-foreground"}>Get in touch</h5>
                     <p className={"text-muted-foreground"}>
@@ -20,7 +22,8 @@ export default function Contact() {
                     </p>
                     <Card>
                         <CardContent className={"flex flex-row gap-4"}>
-                            <div className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
+                            <div
+                                className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
                                 <Phone size={24} className={"text-primary-foreground"}/>
                             </div>
                             <div>
@@ -35,7 +38,8 @@ export default function Contact() {
                     </Card>
                     <Card>
                         <CardContent className={"flex flex-row gap-4"}>
-                            <div className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
+                            <div
+                                className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
                                 <MessageCircle size={24} className={"text-primary-foreground"}/>
                             </div>
                             <div>
@@ -47,7 +51,8 @@ export default function Contact() {
                     </Card>
                     <Card>
                         <CardContent className={"flex flex-row gap-4"}>
-                            <div className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
+                            <div
+                                className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
                                 <Mail size={24} className={"text-primary-foreground"}/>
                             </div>
                             <div>
@@ -60,18 +65,21 @@ export default function Contact() {
                     </Card>
                     <Card>
                         <CardContent className={"flex flex-row gap-4"}>
-                            <div className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
+                            <div
+                                className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
                                 <MapPin size={24} className={"text-primary-foreground"}/>
                             </div>
                             <div>
                                 <h5 className={"text-primary font-semibold"}>Visit Our Office</h5>
-                                <p className={"text-muted-foreground"}>HeartBridge Classic Matrimonial Services123 Wedding Street, Love Plaza Mumbai, Maharashtra 400001 India</p>
+                                <p className={"text-muted-foreground"}>HeartBridge Classic Matrimonial Services123
+                                    Wedding Street, Love Plaza Mumbai, Maharashtra 400001 India</p>
                             </div>
                         </CardContent>
                     </Card>
                     <Card>
                         <CardContent className={"flex flex-row gap-4"}>
-                            <div className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
+                            <div
+                                className={"w-12 h-12 shrink-0 flex items-center justify-center bg-primary rounded-full"}>
                                 <Clock size={24} className={"text-primary-foreground"}/>
                             </div>
                             <div>
@@ -83,12 +91,35 @@ export default function Contact() {
                         </CardContent>
                     </Card>
                 </div>
-                <Card>
+                <Card className={"h-auto self-start"}>
                     <CardHeader>
                         <CardTitle>Send us a message</CardTitle>
                     </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
+                    <CardContent className={"space-y-2 md:space-y-4"}>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"first-name"}>Name</Label>
+                            <Input type={"text"} id={"first-name"} placeholder={"Your first name"}/>
+                        </div>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"last-name"}>Last name</Label>
+                            <Input type={"text"} id={"last-name"} placeholder={"Your last name"}/>
+                        </div>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"email"}>Email</Label>
+                            <Input type={"email"} id={"email"} placeholder={"your.email@domain.com"}/>
+                        </div>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"phone"}>Phone</Label>
+                            <Input type={"tel"} id={"phone"} placeholder={"+91 98765 43210"}/>
+                        </div>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"subject"}>Subject</Label>
+                            <Input type={"text"} id={"subject"} placeholder={"Your subject"}/>
+                        </div>
+                        <div className={"space-y-1 md:space-y-2"}>
+                            <Label htmlFor={"message"}>Message</Label>
+                            <Input type={"textarea"} id={"message"} placeholder={"Your message"}/>
+                        </div>
                     </CardContent>
                     <CardFooter>
                         <Button>Send Message</Button>
