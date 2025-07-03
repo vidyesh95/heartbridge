e5bf44a26c0555fe0ed5b1e4e7103999184a7bb4
vinit-churi <vinitchuri@hotmail.com>
2025-07-04 00:24:45 +0530

    Update Dashboard: add profile browsing section with cards and images
    - Implement profile browsing UI with `Card` component and actions (Like, Bookmark, View)
    - Include 18 new profile image assets in `public` directory

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {Bookmark, Eye, Heart} from "lucide-react";
+
 export default function Dashboard() {
     return (
-        <div className={"pt-14"}>Dashboard</div>
+        <section>
+            <hgroup className={"flex flex-col items-center justify-center space-y-4 text-center pt-18"}>
+                <h3 className={"text-4xl md:text-6xl text-secondary-foreground"}>Browse Profiles</h3>
+                <p className={"w-full max-w-2xl text-muted-foreground"}>
+                    6 profiles match your preferences
+                </p>
+            </hgroup>
+            <Card className={"pt-0"}>
+                <CardHeader className={"px-0"}>
+                    <Image src={"/profile3.avif"} width={256} height={256} alt={"Display Image"} className={"w-64 h-64 object-cover"}/>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter className={"space-x-4"}>
+                    <Button variant={"outline"}><Heart/>Like</Button>
+                    <Button variant={"outline"}><Bookmark/></Button>
+                    <Button variant={"outline"}><Eye/></Button>
+                </CardFooter>
+            </Card>
+        </section>
     )
 }
