2771321e12d863a3bc7ab6a77e888e74abdf1140
vinit-churi <vinitchuri@hotmail.com>
2025-07-04 22:21:16 +0530

    Update HeaderSection: adjust navigation links and replace button with avatar in dashboard header
    - Change "Get Started" link to point to `/dashboard` in home header
    - Replace button with user avatar in dashboard header for improved layout consistency

--- src/components/dashboard/header-section.tsx
+++ src/components/dashboard/header-section.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import {Button} from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 export default function HeaderSection() {
     return (
         <header className={"fixed top-0 left-0 z-50 w-full h-14 flex flex-col items-center justify-center backdrop-blur-xl bg-white/20 dark:bg-black/20 border border-white/20 dark:border-white/20"}>
-            <nav className={"w-full max-w-5xl px-4 md:px-0 flex flex-row items-center justify-between gap-4 md:gap-8"}>
+            <nav className={"w-full px-4 flex flex-row items-center justify-between gap-4 md:gap-8"}>
                 <Link href={"/"} className={"font-semibold text-2xl"}>♡ HeartBridge</Link>
-                <Button asChild>
-                    <Link href={"/"}>Get Started now</Link>
-                </Button>
+                <Avatar className={"w-10 h-10"}>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
             </nav>
         </header>
     )
