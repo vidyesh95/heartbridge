94a0a18ba0ec10725f4d6e7b9f3f7a912c26f82d
vinit-churi <vinitchuri@hotmail.com>
2025-07-10 01:26:58 +0530

    Enhance Profile page: replace `<div>` with `<section>` and `<hgroup>`, add headings and description for better structure and user engagement

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,5 +1,12 @@
 export default function Profile() {
     return (
-        <div>Profile</div>
+        <section>
+            <hgroup className={"flex flex-col items-center justify-center space-y-4 text-center pt-18"}>
+                <h3 className={"text-4xl md:text-6xl text-secondary-foreground"}>Bookmarked Profiles</h3>
+                <p className={"w-full max-w-2xl text-muted-foreground"}>
+                    6 profiles match your preferences
+                </p>
+            </hgroup>
+        </section>
     )
 }
