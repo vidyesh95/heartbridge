90217d65910aeda225319f635cd878421b3b1391
vinit-churi <vinitchuri@hotmail.com>
2025-07-10 00:52:30 +0530

    Enhance Liked and Bookmarked pages with improved layout and descriptive content
    - Replace plain `<div>` elements with structured `<section>` and `<hgroup>` tags
    - Add headings and descriptive text for better user engagement and UI consistency

--- src/app/(dashboard)/dashboard/liked/page.tsx
+++ src/app/(dashboard)/dashboard/liked/page.tsx
@@ -1,5 +1,12 @@
 export default function Liked() {
     return (
-        <div>Liked profiles</div>
+        <section>
+            <hgroup className={"flex flex-col items-center justify-center space-y-4 text-center pt-18"}>
+                <h3 className={"text-4xl md:text-6xl text-secondary-foreground"}>Liked Profiles</h3>
+                <p className={"w-full max-w-2xl text-muted-foreground"}>
+                    6 profiles match your preferences
+                </p>
+            </hgroup>
+        </section>
     )
 }
