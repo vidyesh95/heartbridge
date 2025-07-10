b1889e1f1eab74282086a877dbb6aa666294ce99
vinit-churi <vinitchuri@hotmail.com>
2025-07-10 16:05:52 +0530

    Add dynamic Profile detail page and update Profile page content
    - Add `[id]/page.tsx` to render individual dynamic profiles with `id` parameter
    - Update headings and description in main Profile page for improved clarity

--- src/app/(dashboard)/dashboard/profile/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/profile/[id]/page.tsx
@@ -1,10 +1,11 @@
-export default function Profile() {
+export default async function Profile({ params, }: { params: Promise<{ id: string; }> }) {
+    const {id} = await params
     return (
         <section>
             <hgroup className={"flex flex-col items-center justify-center space-y-4 text-center pt-18"}>
-                <h3 className={"text-4xl md:text-6xl text-secondary-foreground"}>Profiles</h3>
+                <h3 className={"text-4xl md:text-6xl text-secondary-foreground"}>Profile {id}</h3>
                 <p className={"w-full max-w-2xl text-muted-foreground"}>
-                    View profile
+                    viewing dynamic profile
                 </p>
             </hgroup>
         </section>
